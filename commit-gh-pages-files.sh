43b78c78c3d2c085e2d676311bf50d66685ae760
Paul Bernard <paul.bernard@geotoolkit.net>
2018-11-13 11:28:25 +0100

    download hugo binary if necessary for site gen

--- commit-gh-pages-files.sh
+++ commit-gh-pages-files.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-../hugo
+if [ ! -f hugo ];then
+    echo "Download hugo v0.51 ..."
+    wget -O hugo.tgz https://github.com/gohugoio/hugo/releases/download/v0.51/hugo_0.51_Linux-64bit.tar.gz
+    tar xvzf hugo.tgz hugo
+    rm -rf hugo.tgz
+fi
 
+# generate site
+./hugo
+
+# commit to gh pages
 cd public
 git add --all
 git commit -m "Publishing to gh-pages"
 cd ..
+
+
+# push to gh-pages
+#git push upstream gh-pages
